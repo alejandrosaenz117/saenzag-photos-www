c8286af17fe0f2ecc304b09b59f4942c6e08200b
Alejandro Saenz <alejandrosaenz117@gmail.com>
2018-06-21 16:46:46 -0400

    More app.component e2e tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import { browser, element, by } from 'protractor';
 
 describe('saenzag-photos-www App', () => {
   let page: AppPage;
@@ -35,4 +36,115 @@ describe('saenzag-photos-www App', () => {
         expect(res).toContain('/#');
       }))
   })
+
+  /** Navbar */
+
+  it('Should locate the nav bar', () => {
+    page.navigateTo();
+    expect(page.getNavBar()).toBeDefined();
+  });
+
+  // it('Should locate the d bar', () => {
+  //   page.navigateTo();
+  //   element(by.css('li.dropdown > a.dropdown-toggle')).click();
+  //   element(by.linkText('Galleries')).click().then(
+  //   );
+  // });
+
+  it('Should get the Galleries link on the nav bar', () => {
+    page.navigateTo();
+    expect(page.getGalleries().getText()).toEqual('Galleries');
+  });
+
+  it('Should get the Videography link on the nav bar', () => {
+    page.navigateTo();
+    expect(page.getVideography().getText()).toEqual('Videography');
+  });
+
+  it('Should redirect to the Videography page when navbar link is clicked', () => {
+    page.navigateTo();
+    const videography = page.getVideography();
+    videography.click();
+    expect(browser.driver.getCurrentUrl()).toContain('/videography');
+  });
+
+  it('Should get the Professional Services link on the nav bar', () => {
+    page.navigateTo();
+    expect(page.getProserv().getText()).toEqual('Professional Services');
+  });
+
+  it('Should redirect to the Professional Services page when navbar link is clicked', () => {
+    page.navigateTo();
+    const proserv = page.getProserv();
+    proserv.click();
+    expect(browser.driver.getCurrentUrl()).toContain('/proserv');
+  });
+
+  it('Should get the Shop link on the nav bar', () => {
+    page.navigateTo();
+    expect(page.getShop().getText()).toEqual('Shop Prints');
+  });
+
+  it('Should get the About link on the nav bar', () => {
+    page.navigateTo();
+    expect(page.getAbout().getText()).toEqual('About');
+  });
+
+  it('Should redirect to the About page when navbar link is clicked', () => {
+    page.navigateTo();
+    const about = page.getAbout();
+    about.click();
+    expect(browser.driver.getCurrentUrl()).toContain('/about');
+  });
+
+  it('Should get the Contact link on the nav bar', () => {
+    page.navigateTo();
+    expect(page.getContact().getText()).toEqual('Contact');
+  });
+
+  it('Should redirect to the Contact page when navbar link is clicked', () => {
+    page.navigateTo();
+    const contact = page.getContact();
+    contact.click();
+    expect(browser.driver.getCurrentUrl()).toContain('/contact');
+  });
+
+  /** Footer */
+
+  it('Should locate the footer', () => {
+    page.navigateTo();
+    expect(page.getFooter()).toBeDefined();
+  });
+
+  it('Should get the Facebook link on the footer', () => {
+    page.navigateTo();
+    expect(page.getFacebook().getAttribute('href')
+      .then(result => {
+        expect(result).toEqual('https://www.facebook.com/saenzagphotos/')
+      }))
+  });
+
+  it('Should get the Twitter link on the footer', () => {
+    page.navigateTo();
+    expect(page.getTwitter().getAttribute('href')
+      .then(result => {
+        expect(result).toEqual('https://twitter.com/saenzagphotos')
+      }))
+  });
+
+  it('Should get the Instagram link on the footer', () => {
+    page.navigateTo();
+    expect(page.getInstagram().getAttribute('href')
+      .then(result => {
+        expect(result).toEqual('https://www.instagram.com/saenzagphotos/')
+      }))
+  });
+
+  it('Should get the Youtube link on the footer', () => {
+    page.navigateTo();
+    expect(page.getYoutube().getAttribute('href')
+      .then(result => {
+        expect(result).toEqual('https://www.youtube.com/channel/UCNiuhaCrRY_4MgU46-o-0vQ')
+      }))
+  });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,4 +1,4 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ElementFinder } from 'protractor';
 import { elementAt } from 'rxjs/operators';
 
 export class AppPage {
@@ -21,4 +21,53 @@ export class AppPage {
   getHomePageURL() {
     return browser.getCurrentUrl();
   }
+
+  getNavBar(): ElementFinder {
+    return element(by.tagName('nav'));
+  }
+
+  getGalleries(): ElementFinder {
+    return this.getNavBar().all(by.css('li')).get(0);
+  }
+
+  getVideography(): ElementFinder {
+    return this.getNavBar().all(by.css('li')).get(1);
+  }
+
+  getProserv(): ElementFinder {
+    return this.getNavBar().all(by.css('li')).get(2);
+  }
+
+  getShop(): ElementFinder {
+    return this.getNavBar().all(by.css('li')).get(3);
+  }
+
+  getAbout(): ElementFinder {
+    return this.getNavBar().all(by.css('li')).get(4);
+  }
+
+  getContact(): ElementFinder {
+    return this.getNavBar().all(by.css('li')).get(5);
+  }
+
+  getFooter(): ElementFinder {
+    return element(by.tagName('footer'));
+  }
+
+  getFacebook(): ElementFinder {
+    return this.getFooter().all(by.css('a')).get(0);
+  }
+
+  getTwitter(): ElementFinder {
+    return this.getFooter().all(by.css('a')).get(1);
+  }
+
+  getInstagram(): ElementFinder {
+    return this.getFooter().all(by.css('a')).get(2);
+  }
+
+  getYoutube(): ElementFinder {
+    return this.getFooter().all(by.css('a')).get(3);
+  }
+
 }
