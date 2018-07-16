f0c528643f2065a03a14f2682c962b695deb62ca
Alejandro Saenz <alejandrosaenz117@gmail.com>
2018-07-15 22:28:47 -0400

    Update service api variable

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -12,7 +12,7 @@ export class AppService {
 
   constructor(private http: HttpClient) { }
 
-  api = 'http://localhost:5000/api';
+  api = 'https://blooming-reef-68251.herokuapp.com/api';
 
   getLandscapeGallery(): Observable<String[]> {
     return this.http.get<String[]>(`${this.api}/gallery_landscape`)
