696b6fd435c2cf36d85a4bb79ba1032d0779d81d
Alejandro Saenz <alejandrosaenz117@gmail.com>
2019-07-28 15:04:56 -0400

    Issue-32.  Init commit for unit tests

--- src/app/registration/registration.component.spec.ts
+++ src/app/registration/registration.component.spec.ts
@@ -1,7 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../../environments/environment';
+import { RouterTestingModule } from '@angular/router/testing';
 import { RegistrationComponent } from './registration.component';
 import { AppService } from '../app.service';
 import { MockAppService } from '../mock-app.service';
@@ -13,7 +17,14 @@ describe('RegistrationComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [RegistrationComponent],
-      imports: [ReactiveFormsModule, HttpClientTestingModule],
+      imports: [
+        ReactiveFormsModule,
+        HttpClientTestingModule,
+        NgbModule,
+        AngularFireModule.initializeApp(environment.firebaseConfig),
+        AngularFireAuthModule,
+        RouterTestingModule
+      ],
       providers: [{ provide: AppService, useClass: MockAppService }]
     }).compileComponents();
   }));
