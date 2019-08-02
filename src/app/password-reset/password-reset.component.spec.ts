e90479cba58a851a90501f979333c6b44f14df8e
Alejandro Saenz <alejandrosaenz117@gmail.com>
2019-08-01 20:56:31 -0400

    Issue-41.  implemented password reset unit tests

--- src/app/password-reset/password-reset.component.spec.ts
+++ src/app/password-reset/password-reset.component.spec.ts
@@ -1,4 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { FirebaseService } from '../firebase.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { environment } from '../../environments/environment';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { PasswordResetComponent } from './password-reset.component';
 
@@ -8,9 +16,17 @@ describe('PasswordResetComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PasswordResetComponent ]
-    })
-    .compileComponents();
+      declarations: [PasswordResetComponent],
+      imports: [
+        NgbModule,
+        ReactiveFormsModule,
+        AngularFireAuthModule,
+        AngularFireModule.initializeApp(environment.firebaseConfig),
+        HttpClientTestingModule,
+        RouterTestingModule
+      ],
+      providers: [FirebaseService]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,5 +37,33 @@ describe('PasswordResetComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('form invalid when empty', () => {
+    expect(component.passwordResetForm.valid).toBeFalsy();
+  });
+
+  it('should be invalid if email is empty', () => {
+    const email = component.passwordResetForm.controls['email'];
+    expect(email.valid).toBeFalsy();
+  });
+
+  it('should be invalid if email is not correctly formatted', () => {
+    component.passwordResetForm.controls['email'].setValue('test@test..');
+    expect(component.passwordResetForm.controls['email'].valid).toBeFalsy();
+    component.passwordResetForm.controls['email'].setValue('test');
+    expect(component.passwordResetForm.controls['email'].valid).toBeFalsy();
+  });
+
+  it('password reset form form should be valid', () => {
+    expect(component.passwordResetForm.valid).toBeFalsy();
+    component.passwordResetForm.controls['email'].setValue('test@gmail.com');
+    expect(component.passwordResetForm.valid).toBeTruthy();
+  });
+
+  it('password reset form form should not be valid with bad email', () => {
+    expect(component.passwordResetForm.valid).toBeFalsy();
+    component.passwordResetForm.controls['email'].setValue('test@');
+    expect(component.passwordResetForm.valid).toBeFalsy();
   });
 });
